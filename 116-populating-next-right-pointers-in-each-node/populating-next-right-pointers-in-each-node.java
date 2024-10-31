7f6a8e7fadee7f34c2be6dd4f041975753890b45
Rishu Kumar <120789766+rishu0303@users.noreply.github.com>
2024-10-31 13:20:11 +0530

    Time: 1 ms (63.74%) | Memory: 44.2 MB (58.10%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -1,3 +1,58 @@
+// /*
+// // Definition for a Node.
+// class Node {
+//     public int val;
+//     public Node left;
+//     public Node right;
+//     public Node next;
+
+//     public Node() {}
+    
+//     public Node(int _val) {
+//         val = _val;
+//     }
+
+//     public Node(int _val, Node _left, Node _right, Node _next) {
+//         val = _val;
+//         left = _left;
+//         right = _right;
+//         next = _next;
+//     }
+// };
+// */
+
+// class Solution {
+//     public Node connect(Node root) {
+//         Queue<Node> que = new LinkedList<>();
+//         if(root==null){
+//             return root;
+//         }
+//         que.offer(root);
+//         Node temp;
+//         while(!que.isEmpty()){
+//             int nodeCount = que.size();
+//             while(nodeCount-->1){
+//                 temp = que.poll();
+//                 if(temp.left!=null){
+//                     que.offer(temp.left);
+//                 }
+//                 if(temp.right!=null){
+//                     que.offer(temp.right);
+//                 }
+//                 temp.next = que.peek();
+//             }
+//             temp = que.poll();
+//             temp.next = null;
+//             if(temp.left!=null){
+//                     que.offer(temp.left);
+//             }
+//             if(temp.right!=null){
+//                 que.offer(temp.right);
+//             }
+//         }
+//         return root;
+//     }
+// }
 /*
 // Definition for a Node.
 class Node {
@@ -23,33 +78,28 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        Queue<Node> que = new LinkedList<>();
-        if(root==null){
-            return root;
-        }
-        que.offer(root);
-        Node temp;
-        while(!que.isEmpty()){
-            int nodeCount = que.size();
-            while(nodeCount-->1){
-                temp = que.poll();
-                if(temp.left!=null){
-                    que.offer(temp.left);
-                }
-                if(temp.right!=null){
-                    que.offer(temp.right);
-                }
-                temp.next = que.peek();
-            }
-            temp = que.poll();
-            temp.next = null;
-            if(temp.left!=null){
-                    que.offer(temp.left);
-            }
-            if(temp.right!=null){
-                que.offer(temp.right);
-            }
-        }
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+        bfs(queue);
         return root;
+    }
+    public void bfs(Queue<Node> queue){
+        Queue<Node> temp=new LinkedList<>();
+        while(!queue.isEmpty()){
+            Node curr=queue.poll();
+            if(curr==null){
+                continue;
+            }
+            if(curr.left!=null){
+                temp.add(curr.left);
+            }
+            if(curr.right!=null){
+                temp.add(curr.right);
+            }
+            curr.next=queue.peek();
+        }
+        if(!temp.isEmpty()){
+            bfs(temp);
+        }
     }
 }
