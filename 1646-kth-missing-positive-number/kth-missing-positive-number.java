498b6f36c1cf6d1477a6fb5758ab9657fd50b903
Rishu Kumar <120789766+rishu0303@users.noreply.github.com>
2024-11-15 19:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (37.47%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,44 +1,41 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int n = arr.length;
-        boolean[] integer = new boolean[arr[n-1]+1];
-        for(int i = 0;i<n;i++){
-            integer[arr[i]] = true;
-        }
-        int i;
-        for(i=1;i<arr[n-1];i++){
-            if(integer[i]==false){
-                k--;
-                if(k==0)    return i;
+        int low=0, high = arr.length-1;
+
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+
+            int missing = arr[mid] - (mid+1);
+
+            if(missing<k)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
             }
         }
-        while(k>0){
-            i++;
-            k--;
-        }
-        return i;
-        // ArrayList<Integer> list = new ArrayList<>();
-        // int  i = 1;
-        // int res = 1;
-        // while(k > 0){
-        //     if(!search(arr,i)){
-        //         res = i;
-        //         k--;
-        //     }
-        //     i++;
-        // }
-        // return res;
+        return k + high + 1;  
     }
-    // public boolean search(int[] nums, int target) {
-    //     int l = 0;
-    //     int h = nums.length-1;
-    //     int mid;
-    //     while(l<=h){
-    //         mid = (l+h)/2;
-    //         if(nums[mid]==target)   return true;
-    //         else if(nums[mid]>target)   h = mid-1;
-    //         else    l = mid+1;
+    // public int findKthPositive(int[] arr, int k) {
+    //     int n = arr.length;
+    //     boolean[] integer = new boolean[arr[n-1]+1];
+    //     for(int i = 0;i<n;i++){
+    //         integer[arr[i]] = true;
     //     }
-    //     return false;
+    //     int i;
+    //     for(i=1;i<arr[n-1];i++){
+    //         if(integer[i]==false){
+    //             k--;
+    //             if(k==0)    return i;
+    //         }
+    //     }
+    //     while(k>0){
+    //         i++;
+    //         k--;
+    //     }
+    //     return i;
     // }
 }
