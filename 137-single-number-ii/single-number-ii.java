3093951887a3767d56299ed390cc80bfed3aeee4
Rishu Kumar <120789766+rishu0303@users.noreply.github.com>
2024-11-19 00:06:11 +0530

    Time: 4 ms (56.76%) | Memory: 45.4 MB (43.16%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums){
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 1;i<n;i+=3){
+            if(nums[i]!=nums[i-1])  return nums[i-1];
+        }
+        return nums[n-1];
+    }
+    // public int singleNumber(int[] nums) {
+    //     int res = 0;
+    //     for(int i = 0;i<32;i++){
+    //         int count = 0;
+    //         for(int ele:nums){
+    //             if((ele&(1<<i))>0) count++;
+    //         }
+    //         if(count%3==1){
+    //             res = res|(1<<i);
+    //         }
+    //     }
+    //     return res;
+    // }
+
+}
