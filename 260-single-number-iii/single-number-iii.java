478451795dd025f3e4ee11f9ff4ce3e579736cbf
Rishu Kumar <120789766+rishu0303@users.noreply.github.com>
2024-11-19 21:19:55 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (80.82%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,15 +1,33 @@
 class Solution {
-    public int[] singleNumber(int[] nums) {
-        int[] res = new int[2];
-        Arrays.sort(nums);
+    public int[] singleNumber(int[] nums){
         int n = nums.length;
-        int index = 0;
-        for(int i = 0;i<n-1;i++){
-            if(nums[i]!=nums[i+1]){
-                res[index++] = nums[i];
-            }else i++;
+        int xor = 0;
+        for(int i = 0;i<n;i++){
+            xor^=nums[i];
         }
-        if(index!=2) res[1] = nums[n-1];
+        int mask = xor^(xor&(xor-1));
+        int a = 0;
+        int b = 0;
+        for(int i = 0;i<n;i++){
+            if((mask & nums[i]) == 0)  a = a ^ nums[i];
+            else b = b ^ nums[i];
+        }
+        int[] res = new int[2];
+        res[0] = a;
+        res[1] = b;
         return res;
     }
+    // public int[] singleNumber(int[] nums) {
+    //     int[] res = new int[2];
+    //     Arrays.sort(nums);
+    //     int n = nums.length;
+    //     int index = 0;
+    //     for(int i = 0;i<n-1;i++){
+    //         if(nums[i]!=nums[i+1]){
+    //             res[index++] = nums[i];
+    //         }else i++;
+    //     }
+    //     if(index!=2) res[1] = nums[n-1];
+    //     return res;
+    // }
 }
