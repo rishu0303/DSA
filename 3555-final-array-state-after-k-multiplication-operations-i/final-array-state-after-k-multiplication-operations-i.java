4983a1ce2ec1b152f3a33a939a1cd5f89355fdee
Rishu Kumar <120789766+rishu0303@users.noreply.github.com>
2024-12-16 21:01:09 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (50.55%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,15 +1,34 @@
+// class Solution {
+//     public int[] getFinalState(int[] nums, int k, int m) {
+//         int n = nums.length;
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
+//         for(int i = 0;i<n;i++){
+//             pq.add(new int[]{nums[i],i});
+//         }
+//         for(int i = 0; i<k ; i++){
+//             int index = pq.poll()[1];
+//             nums[index]*=m;
+//             pq.add(new int[]{nums[index],index});
+//         }
+//         return nums;
+//     }
+// }
 class Solution {
-    public int[] getFinalState(int[] nums, int k, int m) {
-        int n = nums.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
-        for(int i = 0;i<n;i++){
-            pq.add(new int[]{nums[i],i});
+    public int[] getFinalState(int[] arr, int k, int multiplier) {
+        int ans[]=new int[arr.length];
+        while(k>0)
+        {
+            int x=0;
+            for(int i=1;i<arr.length;i++)
+            {
+                if(arr[i]<arr[x])
+                {
+                    x=i;
+                }
+            }
+            arr[x]=arr[x]*multiplier;
+k--;
         }
-        for(int i = 0; i<k ; i++){
-            int index = pq.poll()[1];
-            nums[index]*=m;
-            pq.add(new int[]{nums[index],index});
-        }
-        return nums;
+        return arr;
     }
 }
